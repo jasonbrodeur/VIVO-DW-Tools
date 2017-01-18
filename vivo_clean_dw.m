bdced2f30404729254198b667d771cbc3f7e4225
Jay Brodeur <jason.brodeur@gmail.com>
2017-01-18 15:33:20 -0500

    updated cleaning script

--- vivo_clean_dw.m
+++ vivo_clean_dw.m
@@ -1,42 +1,51 @@
+%%% vivo_clean_dw.m
+% This script performs cleaning and data normalization procedured for
+% Mosaic DW data exports. 
+%%% Input: 
+% The required input is a tab-separated version of the data extract (no other changes made). 
+% The script also loads in tab-separated lookup table files for faculty
+% positions, departments, faculties and buildings.
+%%% Outputs: 
+% The outputs include a 'cleaned' (ready-for-VIVO-integration) version of
+% the DW data, as well as a data processing report, which indicates
+% specific entries where an inconsistency has been found.
+%
+% Created January 2017 by JJB.
+
+%%% Set the starting path:
 if ispc==1
 start_path = 'D:/Local/VIVO-DW-Tools';
 else
 start_path = '/home/brodeujj/octave/VIVO';
 end
-
 cd(start_path);
 
+%% Open the DW data export, read it and organize data into a cell array
 fid = fopen('MCM_VIVO_ALL_FACULTY-46514.tsv','r');
-
 tline = fgetl(fid);
-%numcols = length(findstr(tline,'\t'))+1;
 numcols2 = length(regexp(tline,'\t'))+1;
 formatspec = repmat('%s',1,numcols2);
-
 C = textscan(fid,formatspec,'Delimiter','\t');
 fclose(fid);
 
+%%% Extract headers
 for i = 1:1:numcols2
 % headers{i,1} = C{1,i}(1,1){1,1};
 headers{i,1} = C{1,i}{1,1};%{1,1};
-
 dw(:,i) = C{1,i}(2:end,1);
 end
 
-%Open a document so that we can track bad data:
+%%%Open a document so that we can track bad data:
 fid_report = fopen('MCM_VIVO_ALL_FACULTY-46514-datareport.txt','w');
 
+% Find columns for macid, first and last names:
 macid_col = find(strcmp(headers,'MAC ID')==1);
 fname_col = find(strcmp(headers,'FirstName')==1);
 lname_col = find(strcmp(headers,'LastName')==1);
 
-% Task 1: Put First and Last Names into Sentence Case
-% Exceptions for capitalization
-%%% following a space
-%%% following a hyphen
-%%% following 'MC' and 'MAC' at the start of a name
+%% Task 1: Put First and Last Names into Sentence Case; macIDs into lower case
 
-%% MAC ID: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% MAC IDs to lowercase: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 for i = 1:1:length(dw(:,macid_col))
 tmp = lower(dw{i,macid_col});
 
@@ -46,7 +55,11 @@ dw{i,macid_col}= tmp;
 %dw{i,fname_col} = regexprep(tmp,'(\<[a-z])','${upper($1)}')
 end
 
-%% First Names: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% First Names to Sentence case: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Exceptions for capitalization
+%%% following a space
+%%% following a hyphen
+
 for i = 1:1:length(dw(:,fname_col))
 tmp = lower(dw{i,fname_col});
 to_upper = 1;
@@ -62,7 +75,12 @@ dw{i,fname_col}= tmp;
 %dw{i,fname_col} = regexprep(tmp,'(\<[a-z])','${upper($1)}')
 end
 
-%% Last Names: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Last Names to sentence case : %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Exceptions for capitalization
+%%% following a space
+%%% following a hyphen
+%%% following 'MC' and 'MAC' at the start of a name
+
 for i = 1:1:length(dw(:,lname_col))
 tmp = lower(dw{i,lname_col});
 to_upper = 1;
@@ -76,7 +94,8 @@ if strncmp(tmp,'mac',3)==1; to_upper = [to_upper; 4];end
 tmp(to_upper) = upper(tmp(to_upper));
 dw{i,lname_col}= tmp;
 end
-%%% Additional cleanup
+
+%%% Additional cleanup for last names
 fprintf(fid_report,'%s\n','IDs requiring last name cleanup')
 % extra space on either side of hyphen:
 extra_space = strfind(dw(:,4),' - ');
@@ -85,21 +104,21 @@ for i = 1:1:length(ind)
 fprintf(fid_report,'%s\n',dw{ind(i),1})
 dw{ind(i),4} = strrep(dw{ind(i),4},' - ','-');
 end
-% extra space on left side of hyphen:
+% remove extra space on left side of hyphen:
 extra_space = strfind(dw(:,4),'- ');
 ind=find(cellfun('isempty',extra_space)==0);
 for i = 1:1:length(ind)
 fprintf(fid_report,'%s\n',dw{ind(i),1})
 dw{ind(i),4} = strrep(dw{ind(i),4},' - ','-');
 end
-% extra space on right side of hyphen:
+% remove extra space on right side of hyphen:
 extra_space = strfind(dw(:,4),' -');
 ind=find(cellfun('isempty',extra_space)==0);
 for i = 1:1:length(ind)
 fprintf(fid_report,'%s\n',dw{ind(i),1})
 dw{ind(i),4} = strrep(dw{ind(i),4},' - ','-');
 end
-% two spaces between names:
+% remove two spaces between names:
 extra_space = strfind(dw(:,4),'  ');
 ind=find(cellfun('isempty',extra_space)==0);
 for i = 1:1:length(ind)
@@ -107,7 +126,7 @@ fprintf(fid_report,'%s\n',dw{ind(i),1})
 dw{ind(i),4} = strrep(dw{ind(i),4},' - ','-');
 end
 
-%% Position Title %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Clean Position Titles -- use lookup table to perform find/replace %%%%%%%%%%%%%
 % load the positions lookup table
 fid_pos = fopen('vivo_lookup_positions.tsv','r');
 hdr_pos = fgetl(fid_pos);
@@ -138,7 +157,7 @@ lookup_match = find(strcmp(D{1,1}(:,1),unique_pos{i,1})==1);
     end
 end
 
-%% Faculty Name %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Faculty Name - lookup table replace %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % load the faculties lookup table
 fid_fac = fopen('vivo_lookup_faculties.tsv','r');
 hdr_pos = fgetl(fid_fac);
@@ -169,7 +188,7 @@ lookup_match = find(strcmp(D{1,1}(:,1),unique_fac{i,1})==1);
     end
 end
 
-%% Department Name %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Department Name - lookup table replace %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % load the departments lookup table
 fid_dept = fopen('vivo_lookup_departments.tsv','r');
 hdr_pos = fgetl(fid_dept);
@@ -238,7 +257,8 @@ for i = 1:1:size(dw,1)
 end
 
 
-%% Final Output:
+%% Write the Final Output:
+
 %%% Close the report:
 fclose(fid_report);
 
